f19b73448601036196b37d4df38f2a2f25eb2254
AllhailLesha <a.goponenko0098@gmail.com>
2023-08-18 19:05:44 +0300

    first commit

---
+++ src/main/java/ru/spring/calculator/domain/Addition.java
@@ -0,0 +1,38 @@
+package ru.spring.calculator.domain;
+
+import java.text.DecimalFormat;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Addition {
+    private double distanceToRestaurant;
+    private double distanceToCustomer;
+
+    public double getDistanceToRestaurant() {
+        return distanceToRestaurant;
+    }
+
+    public double getDistanceToCustomer() {
+        return distanceToCustomer;
+    }
+
+    public void setDistanceToRestaurant(double distanceToRestaurant) {
+        this.distanceToRestaurant = distanceToRestaurant;
+    }
+
+    public void setDistanceToCustomer(double distanceToCustomer) {
+        this.distanceToCustomer = distanceToCustomer;
+    }
+
+    public double getFullDistance() {
+        return distanceToRestaurant + distanceToCustomer;
+    }
+
+    public String getShippingPrice() {
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        return decimalFormat.format(getFullDistance() * 3 * getDemandFactor());
+    }
+
+    public double getDemandFactor() {
+        return ThreadLocalRandom.current().nextDouble(1, 3);
+    }
+}
---
+++ src/test/java/ru/spring/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spring.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
